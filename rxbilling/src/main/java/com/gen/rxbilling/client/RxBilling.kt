aa617d4fd578f8cc35e4388c2f3497c8179d1ff5
Dmytro Ostapovets <dmytro.ostapovets@gen.tech>
2023-07-24 16:28:49 +0300

    billing v6

--- rxbilling/src/main/java/com/gen/rxbilling/client/RxBilling.kt
+++ rxbilling/src/main/java/com/gen/rxbilling/client/RxBilling.kt
@@ -1,8 +1,23 @@
 package com.gen.rxbilling.client
 
 import android.app.Activity
-import com.android.billingclient.api.*
+import com.android.billingclient.api.AcknowledgePurchaseParams
+import com.android.billingclient.api.BillingClient
 import com.android.billingclient.api.BillingClient.FeatureType
+import com.android.billingclient.api.BillingFlowParams
+import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.ConsumeParams
+import com.android.billingclient.api.InAppMessageParams
+import com.android.billingclient.api.InAppMessageResult
+import com.android.billingclient.api.ProductDetails
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.PurchaseHistoryRecord
+import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.QueryProductDetailsParams
+import com.android.billingclient.api.QueryPurchaseHistoryParams
+import com.android.billingclient.api.QueryPurchasesParams
+import com.android.billingclient.api.SkuDetails
+import com.android.billingclient.api.SkuDetailsParams
 import com.gen.rxbilling.connection.BillingClientFactory
 import com.gen.rxbilling.exception.BillingException
 import com.gen.rxbilling.lifecycle.Connectable
@@ -25,6 +40,7 @@ interface RxBilling : Connectable<BillingClient> {
 
     fun getPurchaseHistory(@BillingClient.ProductType skuType: String): Single<List<PurchaseHistoryRecord>>
 
+    @Deprecated("use getProductDetails instead")
     fun getSkuDetails(params: SkuDetailsParams): Single<List<SkuDetails>>
 
     /**
@@ -34,30 +50,43 @@ interface RxBilling : Connectable<BillingClient> {
 
     fun launchFlow(activity: Activity, params: BillingFlowParams): Completable
 
+    fun showInappMessages(
+        activity: Activity,
+        params: InAppMessageParams,
+    ): Single<InAppMessageResult>
+
     fun consumeProduct(params: ConsumeParams): Completable
 
     fun acknowledge(params: AcknowledgePurchaseParams): Completable
 }
 
 class RxBillingImpl(
-        billingFactory: BillingClientFactory,
+    billingFactory: BillingClientFactory,
 ) : RxBilling {
 
     private val updateSubject = PublishSubject.create<PurchasesUpdate>()
 
     private val updatedListener = PurchasesUpdatedListener { result, purchases ->
         val event = when (val responseCode = result.responseCode) {
-            BillingClient.BillingResponseCode.OK -> PurchasesUpdate.Success(responseCode, purchases.orEmpty())
-            BillingClient.BillingResponseCode.USER_CANCELED -> PurchasesUpdate.Canceled(responseCode, purchases.orEmpty())
+            BillingClient.BillingResponseCode.OK -> PurchasesUpdate.Success(
+                responseCode,
+                purchases.orEmpty()
+            )
+
+            BillingClient.BillingResponseCode.USER_CANCELED -> PurchasesUpdate.Canceled(
+                responseCode,
+                purchases.orEmpty()
+            )
+
             else -> PurchasesUpdate.Failed(responseCode, purchases.orEmpty())
         }
         updateSubject.onNext(event)
     }
 
     private val connectionFlowable =
-            Completable.complete()
-                    .observeOn(AndroidSchedulers.mainThread()) // just to be sure billing client is called from main thread
-                    .andThen(billingFactory.createBillingFlowable(updatedListener))
+        Completable.complete()
+            .observeOn(AndroidSchedulers.mainThread()) // just to be sure billing client is called from main thread
+            .andThen(billingFactory.createBillingFlowable(updatedListener))
 
     override fun connect(): Flowable<BillingClient> {
         return connectionFlowable
@@ -86,131 +115,153 @@ class RxBillingImpl(
         return getHistory(skuType)
     }
 
+    @Deprecated("use getProductDetails instead")
     override fun getSkuDetails(params: SkuDetailsParams): Single<List<SkuDetails>> {
         return connectionFlowable
-                .flatMapSingle { client ->
-                    Single.create<List<SkuDetails>> {
-                        client.querySkuDetailsAsync(params) { billingResult, skuDetailsList ->
-                            if (it.isDisposed) return@querySkuDetailsAsync
-                            val responseCode = billingResult.responseCode
-                            if (isSuccess(responseCode)) {
-                                it.onSuccess(skuDetailsList.orEmpty())
-                            } else {
-                                it.onError(BillingException.fromResult(billingResult))
-                            }
+            .flatMapSingle { client ->
+                Single.create<List<SkuDetails>> {
+                    client.querySkuDetailsAsync(params) { billingResult, skuDetailsList ->
+                        if (it.isDisposed) return@querySkuDetailsAsync
+                        val responseCode = billingResult.responseCode
+                        if (isSuccess(responseCode)) {
+                            it.onSuccess(skuDetailsList.orEmpty())
+                        } else {
+                            it.onError(BillingException.fromResult(billingResult))
                         }
                     }
-                }.firstOrError()
+                }
+            }.firstOrError()
     }
 
     override fun getProductDetails(params: QueryProductDetailsParams): Single<List<ProductDetails>> {
         return connectionFlowable
-                .flatMapSingle { client ->
-                    Single.create<List<ProductDetails>> {
-                        client.queryProductDetailsAsync(params) { billingResult, skuDetailsList ->
-                            if (it.isDisposed) return@queryProductDetailsAsync
-                            val responseCode = billingResult.responseCode
-                            if (isSuccess(responseCode)) {
-                                it.onSuccess(skuDetailsList)
-                            } else {
-                                it.onError(BillingException.fromResult(billingResult))
-                            }
+            .flatMapSingle { client ->
+                Single.create<List<ProductDetails>> {
+                    client.queryProductDetailsAsync(params) { billingResult, skuDetailsList ->
+                        if (it.isDisposed) return@queryProductDetailsAsync
+                        val responseCode = billingResult.responseCode
+                        if (isSuccess(responseCode)) {
+                            it.onSuccess(skuDetailsList)
+                        } else {
+                            it.onError(BillingException.fromResult(billingResult))
                         }
                     }
-                }.firstOrError()
+                }
+            }.firstOrError()
     }
 
     override fun launchFlow(activity: Activity, params: BillingFlowParams): Completable {
         return connectionFlowable
-                .flatMap {
-                    val responseCode = it.launchBillingFlow(activity, params)
-                    return@flatMap Flowable.just(responseCode)
+            .flatMap {
+                val responseCode = it.launchBillingFlow(activity, params)
+                return@flatMap Flowable.just(responseCode)
+            }
+            .firstOrError()
+            .flatMapCompletable {
+                return@flatMapCompletable if (isSuccess(it.responseCode)) {
+                    Completable.complete()
+                } else {
+                    Completable.error(BillingException.fromResult(it))
                 }
-                .firstOrError()
-                .flatMapCompletable {
-                    return@flatMapCompletable if (isSuccess(it.responseCode)) {
-                        Completable.complete()
-                    } else {
-                        Completable.error(BillingException.fromResult(it))
+            }
+    }
+
+    override fun showInappMessages(
+        activity: Activity,
+        params: InAppMessageParams,
+    ): Single<InAppMessageResult> {
+        return connectionFlowable
+            .flatMapSingle { client ->
+                Single.create<InAppMessageResult> {
+                    client.showInAppMessages(activity, params) { result: InAppMessageResult ->
+                        if (it.isDisposed) return@showInAppMessages
+                        val responseCode = result.responseCode
+                        if (isSuccess(responseCode)) {
+                            it.onSuccess(result)
+                        } else {
+                            it.onError(BillingException.fromResponseCode(result.responseCode))
+                        }
                     }
                 }
+            }
+            .firstOrError()
     }
 
     override fun consumeProduct(params: ConsumeParams): Completable {
         return connectionFlowable
-                .flatMapSingle { client ->
-                    Single.create<Int> {
-                        client.consumeAsync(params) { result, _ ->
-                            if (it.isDisposed) return@consumeAsync
-                            val responseCode = result.responseCode
-                            if (isSuccess(responseCode)) {
-                                it.onSuccess(responseCode)
-                            } else {
-                                it.onError(BillingException.fromResult(result))
-                            }
+            .flatMapSingle { client ->
+                Single.create<Int> {
+                    client.consumeAsync(params) { result, _ ->
+                        if (it.isDisposed) return@consumeAsync
+                        val responseCode = result.responseCode
+                        if (isSuccess(responseCode)) {
+                            it.onSuccess(responseCode)
+                        } else {
+                            it.onError(BillingException.fromResult(result))
                         }
                     }
                 }
-                .firstOrError()
-                .ignoreElement()
+            }
+            .firstOrError()
+            .ignoreElement()
     }
 
     override fun acknowledge(params: AcknowledgePurchaseParams): Completable {
         return connectionFlowable
-                .flatMapSingle { client ->
-                    Single.create<Int> {
-                        client.acknowledgePurchase(params) { result ->
-                            if (it.isDisposed) return@acknowledgePurchase
-                            val responseCode = result.responseCode
-                            if (isSuccess(responseCode)) {
-                                it.onSuccess(responseCode)
-                            } else {
-                                it.onError(BillingException.fromResult(result))
-                            }
+            .flatMapSingle { client ->
+                Single.create<Int> {
+                    client.acknowledgePurchase(params) { result ->
+                        if (it.isDisposed) return@acknowledgePurchase
+                        val responseCode = result.responseCode
+                        if (isSuccess(responseCode)) {
+                            it.onSuccess(responseCode)
+                        } else {
+                            it.onError(BillingException.fromResult(result))
                         }
                     }
                 }
-                .firstOrError()
-                .ignoreElement()
+            }
+            .firstOrError()
+            .ignoreElement()
     }
 
     private fun getBoughtItems(@BillingClient.ProductType type: String): Single<List<Purchase>> {
         return connectionFlowable
-                .flatMapSingle {
-                    Single.create<List<Purchase>> { emitter ->
-                        val params = QueryPurchasesParams.newBuilder()
-                                .setProductType(type)
-                                .build()
-                        it.queryPurchasesAsync(params) { billingResult, mutableList ->
-                            if (emitter.isDisposed) return@queryPurchasesAsync
-                            if (isSuccess(billingResult.responseCode)) {
-                                emitter.onSuccess(mutableList)
-                            } else {
-                                emitter.onError(BillingException.fromResult(billingResult))
-                            }
+            .flatMapSingle {
+                Single.create<List<Purchase>> { emitter ->
+                    val params = QueryPurchasesParams.newBuilder()
+                        .setProductType(type)
+                        .build()
+                    it.queryPurchasesAsync(params) { billingResult, mutableList ->
+                        if (emitter.isDisposed) return@queryPurchasesAsync
+                        if (isSuccess(billingResult.responseCode)) {
+                            emitter.onSuccess(mutableList)
+                        } else {
+                            emitter.onError(BillingException.fromResult(billingResult))
                         }
                     }
-                }.firstOrError()
+                }
+            }.firstOrError()
     }
 
     private fun getHistory(@BillingClient.ProductType type: String): Single<List<PurchaseHistoryRecord>> {
         return connectionFlowable
-                .flatMapSingle { client ->
-                    Single.create<List<PurchaseHistoryRecord>> {
-                        val params = QueryPurchaseHistoryParams.newBuilder()
-                                .setProductType(type)
-                                .build()
-                        client.queryPurchaseHistoryAsync(params) { billingResult: BillingResult, list: MutableList<PurchaseHistoryRecord>? ->
-                            if (it.isDisposed) return@queryPurchaseHistoryAsync
-                            val responseCode = billingResult.responseCode
-                            if (isSuccess(responseCode)) {
-                                it.onSuccess(list.orEmpty())
-                            } else {
-                                it.onError(BillingException.fromResult(billingResult))
-                            }
+            .flatMapSingle { client ->
+                Single.create<List<PurchaseHistoryRecord>> {
+                    val params = QueryPurchaseHistoryParams.newBuilder()
+                        .setProductType(type)
+                        .build()
+                    client.queryPurchaseHistoryAsync(params) { billingResult: BillingResult, list: MutableList<PurchaseHistoryRecord>? ->
+                        if (it.isDisposed) return@queryPurchaseHistoryAsync
+                        val responseCode = billingResult.responseCode
+                        if (isSuccess(responseCode)) {
+                            it.onSuccess(list.orEmpty())
+                        } else {
+                            it.onError(BillingException.fromResult(billingResult))
                         }
                     }
-                }.firstOrError()
+                }
+            }.firstOrError()
     }
 
     private fun isSuccess(responseCode: Int): Boolean {
--- rxbilling/src/main/java/com/gen/rxbilling/exception/BillingException.kt
+++ rxbilling/src/main/java/com/gen/rxbilling/exception/BillingException.kt
@@ -3,36 +3,134 @@ package com.gen.rxbilling.exception
 import com.android.billingclient.api.BillingClient
 import com.android.billingclient.api.BillingResult
 
-sealed class BillingException(result: BillingResult) :
-        Exception("Billing error, code ${result.responseCode}\n${result.debugMessage}") {
+sealed class BillingException(
+    responseCode: Int,
+    debugMessage: String? = null,
+) : Exception("Billing error, code $responseCode\n$debugMessage") {
 
     companion object {
         fun fromResult(result: BillingResult): BillingException {
-            return when (result.responseCode) {
-                BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED -> FeatureNotSupportedException(result)
-                BillingClient.BillingResponseCode.SERVICE_DISCONNECTED -> ServiceDisconnectedException(result)
-                BillingClient.BillingResponseCode.USER_CANCELED -> UserCanceledException(result)
-                BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE -> ServiceUnavailableException(result)
-                BillingClient.BillingResponseCode.BILLING_UNAVAILABLE -> BillingUnavailableException(result)
-                BillingClient.BillingResponseCode.ITEM_UNAVAILABLE -> ItemUnavailableException(result)
-                BillingClient.BillingResponseCode.DEVELOPER_ERROR -> DeveloperErrorException(result)
-                BillingClient.BillingResponseCode.ERROR -> FatalException(result)
-                BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED -> AlreadyOwnedException(result)
-                BillingClient.BillingResponseCode.ITEM_NOT_OWNED -> NotOwnedException(result)
-                else -> UnknownException(result)
+            return fromResponseCode(result.responseCode, result.debugMessage)
+        }
+
+        fun fromResponseCode(responseCode: Int, message: String? = null): BillingException {
+            return when (responseCode) {
+                BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED ->
+                    FeatureNotSupportedException(responseCode, message)
+
+                BillingClient.BillingResponseCode.SERVICE_DISCONNECTED ->
+                    ServiceDisconnectedException(responseCode, message)
+
+                BillingClient.BillingResponseCode.USER_CANCELED ->
+                    UserCanceledException(
+                        responseCode,
+                        message,
+                    )
+
+                BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE ->
+                    ServiceUnavailableException(
+                        responseCode,
+                        message,
+                    )
+
+                BillingClient.BillingResponseCode.BILLING_UNAVAILABLE ->
+                    BillingUnavailableException(responseCode, message)
+
+                BillingClient.BillingResponseCode.ITEM_UNAVAILABLE ->
+                    ItemUnavailableException(
+                        responseCode,
+                        message,
+                    )
+
+                BillingClient.BillingResponseCode.DEVELOPER_ERROR ->
+                    DeveloperErrorException(
+                        responseCode,
+                        message,
+                    )
+
+                BillingClient.BillingResponseCode.ERROR ->
+                    FatalException(responseCode, message)
+
+                BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED ->
+                    AlreadyOwnedException(
+                        responseCode,
+                        message,
+                    )
+
+                BillingClient.BillingResponseCode.ITEM_NOT_OWNED ->
+                    NotOwnedException(
+                        responseCode,
+                        message,
+                    )
+
+                BillingClient.BillingResponseCode.NETWORK_ERROR ->
+                    NetworkException(
+                        responseCode,
+                        message,
+                    )
+
+                else -> UnknownException(responseCode, message)
             }
         }
     }
 
-    class FeatureNotSupportedException(result: BillingResult) : BillingException(result)
-    class ServiceDisconnectedException(result: BillingResult) : BillingException(result)
-    class UserCanceledException(result: BillingResult) : BillingException(result)
-    class ServiceUnavailableException(result: BillingResult) : BillingException(result)
-    class BillingUnavailableException(result: BillingResult) : BillingException(result)
-    class ItemUnavailableException(result: BillingResult) : BillingException(result)
-    class DeveloperErrorException(result: BillingResult) : BillingException(result)
-    class FatalException(result: BillingResult) : BillingException(result)
-    class AlreadyOwnedException(result: BillingResult) : BillingException(result)
-    class NotOwnedException(result: BillingResult) : BillingException(result)
-    class UnknownException(result: BillingResult) : BillingException((result))
+    class FeatureNotSupportedException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class ServiceDisconnectedException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class UserCanceledException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class ServiceUnavailableException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class BillingUnavailableException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class ItemUnavailableException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class DeveloperErrorException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class FatalException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class AlreadyOwnedException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class NotOwnedException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class NetworkException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
+
+    class UnknownException(
+        responseCode: Int,
+        message: String? = null,
+    ) : BillingException(responseCode, message)
 }
